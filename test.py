ff97dd4e596b12a3c729adac1b0d55f57a308e0a
openough <openoughlst@gmail.com>
2023-10-01 14:09:19 +0300

    edit test.py

--- test.py
+++ test.py
@@ -1,31 +1,11 @@
-def generate_strings(q, w, e):
-    def generate_helper(current_string, remaining_q, remaining_w, remaining_e):
-        # Если все символы использованы, добавляем текущую строку в результат
-        if not remaining_q and not remaining_w and not remaining_e:
-            result.append(current_string)
-            return
+a = input()
+output = ""
+for i in range(len(str(a))):
+    for j in range(len(str(a))):
+        for k in range(len(str(a))):
+            for t in range(len(str(a))):
+                output += f'{a[i]}{a[j]}{a[k]} -- "{a[t]}" --> '
+print(output)
+ 
 
-        # Если есть доступные символы q, добавляем их к текущей строке и рекурсивно вызываем функцию
-        if remaining_q:
-            generate_helper(current_string + 'q', remaining_q - 1, remaining_w, remaining_e)
-
-        # То же самое для символов w и e
-        if remaining_w:
-            generate_helper(current_string + 'w', remaining_q, remaining_w - 1, remaining_e)
-
-        if remaining_e:
-            generate_helper(current_string + 'e', remaining_q, remaining_w, remaining_e - 1)
-
-    result = []
-    generate_helper('', q, w, e)
-    return result
-
-# Пример использования
-q = 1
-w = 1
-e = 1
-generated_strings = generate_strings(q, w, e)
-print(generated_strings)
-
-
-#ЭТОТ КОД НЕ РАБОТАЕТ))
+ #IDK HOW TO MAKE IT ORGERRED))
